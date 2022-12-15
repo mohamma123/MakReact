836f9e1cb2fdb352d6eb9d031dce22d7beecaabf
mohamma123 <mo.mohebali@gmail.com>
2022-12-16 01:06:08 +0330

    Updates

---
+++ precache-manifest.2eeb2ebe80ddf669948704e19d3f7bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f3a7f1ea353cb4e7737",
+    "url": "/mohamma123/MakReact/static/css/main.bd0a083e.chunk.css"
+  },
+  {
+    "revision": "5f3a7f1ea353cb4e7737",
+    "url": "/mohamma123/MakReact/static/js/main.5f3a7f1e.chunk.js"
+  },
+  {
+    "revision": "037611408a2d7d3f6546",
+    "url": "/mohamma123/MakReact/static/css/1.93753a8a.chunk.css"
+  },
+  {
+    "revision": "037611408a2d7d3f6546",
+    "url": "/mohamma123/MakReact/static/js/1.03761140.chunk.js"
+  },
+  {
+    "revision": "d0029d21ca26ac216754",
+    "url": "/mohamma123/MakReact/static/js/runtime~main.d0029d21.js"
+  },
+  {
+    "revision": "8c1abbda23687f89f446e835d327af1b",
+    "url": "/mohamma123/MakReact/index.html"
+  }
+];
